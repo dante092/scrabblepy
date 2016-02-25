4c34783b6474595a519fa76d0d0e5d767ed17dde
dante092 <gregborrelly@gmail.com>
2016-02-25 08:35:47 -0500

    Update scrabble.py

--- scrabble.py
+++ scrabble.py
@@ -92,6 +92,19 @@ class SrabblePy:
     def display_word(matches):
         for word in matches: 
             print(" - " + word)
+            
+    def score_words(matches):
+        scores = {"A": 1, "C": 3, "B": 3, "E": 1, "D": 2, "G": 2, 
+             "F": 4, "I": 1, "H": 4, "K": 5, "J": 8, "M": 3, 
+             "L": 1, "O": 1, "N": 1, "Q": 10, "P": 3, "S": 1, 
+             "R": 1, "U": 1, "T": 1, "W": 4, "V": 4, "Y": 4, 
+             "X": 8, "Z": 10} 
+
+        for word in matches:
+            word_score = 0 
+            for letter in word:
+                word_score = word_score + scores[letter]
+                
      
 
 
